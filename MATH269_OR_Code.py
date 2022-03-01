7dce90d5e5100008c18fa8a32ac14d0b94dd98aa
Luminousking <wxy18362037636@outlook.com>
2022-03-01 19:54:50 +0000

    Update MATH269_OR_Code.py

--- MATH269_OR_Code.py
+++ MATH269_OR_Code.py
@@ -4,6 +4,45 @@ Created on Tue Mar  1 16:53:49 2022
 
 @author: Luminous Isaac
 """
+######################## Linear Programming Plt #############################
+
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+#from pylab import figure, text, scatter, show
+ 
+# Construct lines
+ 
+x = np.linspace(0.0,50,100)
+ 
+y1 = 3.0*x - 33
+ 
+y2 = (-1/15)*x + 15
+ 
+y3 = (14/5)*x - 28
+ 
+# Make plot
+plt.plot(x, y1, label=r'$y1 = 3.0*x - 33$')
+plt.plot(x, y2, label=r'$y2 = (-1/15)*x + 15$')
+plt.plot(x, y3, label=r'$y3 = (14/5)*x - 28$')
+ 
+plt.xlim((0.0, 50.000))
+plt.ylim((0.0, 50.000))
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y$')
+ 
+# Fill feasible region
+y5 = np.minimum(y1, y3)                                                                                                                                                                                                             
+#y6 = np.maximum(y1, y3)
+plt.fill_between(x, y5, y2, where=y5>y2, color='grey', alpha=0.5)
+ 
+ 
+plt.grid(True, linestyle='-.')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+ 
+#plt.savefig('ordering_constraints.eps',dpi=600,bbox_inches='tight')
+plt.show()
+
 ######################## Hadley-Whitin Algorithm ############################
 from sympy import *
 import math
