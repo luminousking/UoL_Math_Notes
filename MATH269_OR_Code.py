8f555a8f10e05f525517f3c75e6f54f40a171b9b
Luminousking <wxy18362037636@outlook.com>
2022-03-01 19:30:17 +0000

    Update MATH269_OR_Code.py

--- MATH269_OR_Code.py
+++ MATH269_OR_Code.py
@@ -8,20 +8,12 @@ Created on Tue Mar  1 16:53:49 2022
 from sympy import *
 import math
 
-def f(x):
-    
-    if 0<=x<=30:
-        re = 1/30 ## change
-    else:
-        re = 0
-        
-    return re
 
 def expectation():
     
     x = Symbol('x')
     
-    Ex = integrate(x*1/30, (x, 0, 30)) ##change
+    Ex = integrate(x*1/32, (x, 0, 32)) ##change
     
     return Ex
 
@@ -35,21 +27,22 @@ def HWA(D, K, h, p, n):
     else:
         
         y = math.sqrt(2 * D * K / h)
-        R = 30*(1 - y * h / (p * D)) ## change
+        R = 32*(1 - y * h / (p * D)) ## change
         
         if n == 1:    
             return y, R
         else:
             for i in range(2, n+1):
                 x = Symbol('x')
-                S_R = integrate((x - R) / 30, (x, R, 30)) ##change
+                S_R = integrate((x - R) / 32, (x, R, 32)) ##change
                 y = math.sqrt((2 * D * (K + p * S_R)) / h)
-                R = 30*(1 - y * h / (p * D)) ## change
+                R = 32*(1 - y * h / (p * D)) ## change
                 
-    return y, R
+    return y_hat, y_wave, y, R
     
     
-HWA(17, 3, 1, 2, 4)
+HWA(30, 4, 0.48, 1.11, 3)
+
 
 
 
